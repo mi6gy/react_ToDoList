795283176f73f7fcea5d302f14acabd5f3f90012
Miguel Jimenez <mjimenez08@gmail.com>
2020-10-12 22:25:53 -0400

    added todo.js, added buttons and styling, updated input text handler correctly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css';
 import Form from  "./components/Form";
+import ToDoList from "./components/ToDoList";
 
 function App() {
+  const[inputText, setInputText] = useState ("");
+  const [todos, setTodos] = useState ([]);
+
   return (
     <div className="App">
       <header>
         <h1>Hello world</h1>
         </header>
-      <Form/>
+      <Form todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      inputText={inputText}
+      />
+      <ToDoList/>
     </div>
   );
 }
