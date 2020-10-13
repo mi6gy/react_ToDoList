9b0dc19d9135647094a4b20018086e75ba81a66e
Miguel Jimenez <mjimenez08@gmail.com>
2020-10-12 20:06:20 -0400

    added components FORM and LIST

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from  "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header>
+        <h1>Hello world</h1>
+        </header>
+      <Form/>
     </div>
   );
 }
