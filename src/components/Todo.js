74bbccb2a4414f1e754f6fdff094ef7332bfab32
Miguel Jimenez <mjimenez08@gmail.com>
2020-10-13 00:18:53 -0400

    added completed function, toggle, and line through

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,26 @@
 import React from "react";
 
-const Todo = ({ text,todo,setTodos,todos }) => {
+const Todo = ({ text, todo, setTodos, todos }) => {
     //Events: Delete
 
-const deleteHandler = () =>{
-setTodos(todos.filter((el) => el.id !== todo.id));
-}
+    const deleteHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    }
 
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                };
+            }
+            return item;
+        }))
+    }
     return (
         <div className="todo">
-            <li className="todo-items">{text}</li>
-            <button className="complete-btn">
+            <li className={`todo-items ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
