1810bacb935d8a8760ee4067ef4acc5959fcde56
Miguel Jimenez <mjimenez08@gmail.com>
2020-10-22 15:11:03 -0400

    spelling error

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>To Do Lists:</h1>
+        <h1>To Do List:</h1>
       </header>
       <Form todos={todos}
         setTodos={setTodos}
